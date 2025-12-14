e33a58e696ed6d35e8afefd8cba6604b379caa3c
izabelabf <izabelabrito004@gmail.com>
2025-12-14 18:07:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (pageYOffset >= sectionTop - window.innerHeight / 2) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
